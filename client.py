f1f1e000c7eca25f0c247efdc394d1d84f70620c
Yunis Nabiyev <yunisnabiyev@gmail.com>
2025-03-21 14:36:13 -0700

    face rec working

--- client.py
+++ client.py
@@ -4,20 +4,19 @@ import facemanager
 
 class Client: #TODO make singleton
     class params:
-        SHOW_LIVE_FEED = True
+        SHOW_LIVE_FEED = True #whether webcam should be shown, or just the faces as captured
         FRAME_WIDTH = 1280
         FRAME_HEIGHT = 720
         FRAME_FPS = 10 #may not work
-        FRAME_SKIP = 1 #skip every xth frame
+        FRAME_SKIP = 1 #skip x frames, process x+1
         FRAME_COUNTER = 0
-        SCALE_FACTOR = 0.75  # Scale down for faster processing
-        RETRY_TIMEOUT = 10
-        DB_PATH = "Database/encodings.json"
+        RETRY_TIMEOUT = 10 #TODO used with async.timeout
+        DB_PATH = "DB/encoding"
         STOP_TIME = ["11:59", "12:59", "13:59", "14:59", "15:59", "16:59", "17:59"] #when client should shut down??? TODO remove this lol. If necessary make program self destruct after x time
 
     def __init__(self):
         self.current_frame = None
-        self.face_manager = facemanager.FaceManager()
+        self.face_manager = facemanager.FaceManager() #TODO remove, instead query user on startup ip of facemanager
         #self.tts_manager = ttsmanager.TTSManager()
         self.window = cv2.VideoCapture(0, cv2.CAP_ANY) #open video input(index 0), and auto detect input type(CAP_ANY)
         self.set_window()
@@ -26,7 +25,7 @@ class Client: #TODO make singleton
         #del self.tts_manager
         self.window.release()
         cv2.destroyAllWindows()
-        #del self.face_manager
+        del self.face_manager
 
     def get_frame(self):
         has_frame, frame = self.window.read()
@@ -36,47 +35,39 @@ class Client: #TODO make singleton
         self.window.set(cv2.CAP_PROP_FRAME_WIDTH, self.params.FRAME_WIDTH) #window width
         self.window.set(cv2.CAP_PROP_FRAME_HEIGHT, self.params.FRAME_HEIGHT) #window height
         self.window.set(cv2.CAP_PROP_FPS, self.params.FRAME_FPS) #window framerate
-        self.window.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.75) # set auto exposure factor TODO necessary?
+        self.window.set(cv2.CAP_PROP_AUTO_EXPOSURE, 3) # set auto exposure factor TODO necessary?
 
     def draw_face_box(self, location, name):
         (x, y, w, h) = location
-        x = int(x / self.params.SCALE_FACTOR)
-        y = int(y / self.params.SCALE_FACTOR)
-        w = int(w / self.params.SCALE_FACTOR)
-        h = int(h / self.params.SCALE_FACTOR)
-
         cv2.rectangle(self.current_frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
         cv2.rectangle(self.current_frame, (x, y), (x+w, y-35), (0, 0, 255), cv2.FILLED)
         cv2.putText(self.current_frame, name, (x, y), cv2.FONT_HERSHEY_DUPLEX, 1.25, (255, 255, 255), 1, bottomLeftOrigin=False)
 
     def show_face_crop(self, location):
         (x, y, w, h) = location
-        x = int(x / self.params.SCALE_FACTOR)
-        y = int(y / self.params.SCALE_FACTOR)
-        w = int(w / self.params.SCALE_FACTOR)
-        h = int(h / self.params.SCALE_FACTOR)
         cv2.imshow(f"{x} {y} {w} {h}", self.current_frame[y:y+h, x:x+w])
 
     def cycle(self):
-        if self.params.FRAME_COUNTER >= self.params.FRAME_SKIP and self.params.FRAME_SKIP != 0:
+        #apply frame skip
+        if self.params.FRAME_COUNTER % self.params.FRAME_SKIP != 0:
             self.params.FRAME_COUNTER = 0
             return
         self.params.FRAME_COUNTER += 1
-
+        
         self.get_frame()
 
-        small_frame = cv2.resize(self.current_frame, (0,0), fx=self.params.SCALE_FACTOR, fy=self.params.SCALE_FACTOR)#automatically scale down image by (fx, fy)
-        
-        for face in self.face_manager.find_faces(small_frame):
+        #signal face manager to find faces
+        for face in self.face_manager.find_faces(self.current_frame):
             self.draw_face_box(face.location, face.name)
             if not self.params.SHOW_LIVE_FEED:
-                self.show_face_crop(face.location)
+                self.show_face_crop(face.location) #TODO needs to be implemented better than just spam
             #do whatever else with the face identity, like tts
         
         if self.params.SHOW_LIVE_FEED:
             cv2.imshow('Video', self.current_frame) #display video input + changes to monitor
         cv2.waitKey(1)
         self.frame = None
+        #TODO web ui that shows live feed AND most recent faces
 
 if __name__ == "__main__":
     client = Client()
--- facemanager.py
+++ facemanager.py
@@ -4,6 +4,8 @@ import cv2 #for facial recognition
 import face_recognition #for facial recognition and encodings
 import os #for finding files
 
+#TODO Web UI for Audit
+
 class FaceManager: #TODO make singleton
     class Face:
         def __init__(self, p_name, p_location):
@@ -11,20 +13,28 @@ class FaceManager: #TODO make singleton
             self.location = p_location
 
     class params:
-        FACE_IDS_PATH = ""
-        AUDIT_FACES_PATH = ""
+        FRAME_SCALE_FACTOR = 0.75  # Scale down for faster processing
+        IDS_ENCODINGS_PATH = "db/encodings"
+        FACE_IDS_PATH = "DB/faceids"
+        AUDIT_FACES_PATH = "DB/audit"
         CONFIDENCE_TOLERANCE = 50 #how confident should match be to accept 
         ENCODING_NUM_JITTERS = 1 #increase to resample more times and thus make encodings more accurate, runs slower though
         ENCODING_MATCH_TOLERANCE = 0.6 #how far apart should encodings be to qualify as matches
 
-    face_ids = {} # {encoding : label}
-    faces_to_audit = {} # {encoding : image to audit}
+    id_to_encoding = {} #{ID : encoding}
+    id_to_label = {} # {ID : label}
+    faces_to_audit = {} # {ID : image to audit}
 
     def __init__(self):
+        #load Encoding to ID DB
+        if os.path.exists(self.params.IDS_ENCODINGS_PATH):
+            with open(self.params.IDS_ENCODINGS_PATH, "rt") as f:
+                self.id_to_label = json.load(f)
+
         #load ID to Label DB
         if os.path.exists(self.params.FACE_IDS_PATH):
             with open(self.params.FACE_IDS_PATH, "rt") as f:
-                self.face_ids = json.load(f)
+                self.id_to_label = json.load(f)
 
         #load faces to audit
         if os.path.exists(self.params.AUDIT_FACES_PATH):
@@ -33,9 +43,17 @@ class FaceManager: #TODO make singleton
 
     def __del__(self):
         #save ID to Label DB
-        if len(self.face_ids) > 0:
+        if len(self.id_to_encoding) > 0:
+            with open(self.params.IDS_ENCODINGS_PATH, "wt") as f:
+                json.dump(self.id_to_encoding, f)
+        else:
+            if os.path.exists(self.params.IDS_ENCODINGS_PATH):  
+                os.remove(self.params.IDS_ENCODINGS_PATH)
+
+        #save ID to Label DB
+        if len(self.id_to_label) > 0:
             with open(self.params.FACE_IDS_PATH, "wt") as f:
-                json.dump(self.face_ids, f)
+                json.dump(self.id_to_label, f)
         else:
             if os.path.exists(self.params.FACE_IDS_PATH):
                 os.remove(self.params.FACE_IDS_PATH)
@@ -49,13 +67,16 @@ class FaceManager: #TODO make singleton
                 os.remove(self.params.AUDIT_FACES_PATH)
 
     def find_faces(self, frame) -> [Face]:
-        #TODO scale frame serverside rather than client side
+        scaled_frame = cv2.resize(frame, (0,0), fx=self.params.FRAME_SCALE_FACTOR, fy=self.params.FRAME_SCALE_FACTOR)#automatically scale down image by (fx, fy)
+        #TODO make async so that faces are sent as they are identified. Only useful for multiple faces
         faces = []
         #TODO more complicated model first that can detect many angles
 
         #frontal only face_recognition as fallback
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_rgb = cv2.cvtColor(scaled_frame, cv2.COLOR_BGR2RGB)
         for loc in face_recognition.face_locations(frame_rgb):
+            (t,r,b,l) = loc
+            loc = (l, t, r-l, b-t) #x,y,w,h
             faces.append(self.identify_face_fallback(frame_rgb, loc))
         return faces
         
@@ -67,20 +88,26 @@ class FaceManager: #TODO make singleton
         #    return FaceManager.Face(label, ?)
 
     def identify_face_fallback(self, frame_rgb, location) -> Face:
-        (y,w,h,x) = location
+        (x,y,w,h) = location
+        x = int(x / self.params.FRAME_SCALE_FACTOR)
+        y = int(y / self.params.FRAME_SCALE_FACTOR)
+        w = int(w / self.params.FRAME_SCALE_FACTOR)
+        h = int(h / self.params.FRAME_SCALE_FACTOR)
         encoding = face_recognition.face_encodings(frame_rgb, [location])[0]
-        if str(encoding) in self.face_ids.keys():#if exact match
-            # add new image to train model on
-            return FaceManager.Face(self.face_ids[str(encoding)], (x,y,w,h))
-        #TODO compare encodings for audited close match
-        # for i,match in enumerate(face_recognition.compare_faces(self.face_ids.keys(),encoding,self.params.ENCODING_MATCH_TOLERANCE)):
-        #     if match: #if close match
-        #         #add new image to train model on
-        #         return FaceManager.Face(self.face_ids.values()[i], (x,y,w,h))
-        elif str(encoding) in self.faces_to_audit.keys(): #exact match found pending audit
-            return FaceManager.Face(str(encoding), (x,y,w,h))
+        #TODO remove below. not likely to have exact matches?
+        # if hash(str(encoding)) in self.id_to_label.keys():#convert encoding to ID, look for exact match
+        #     # add new image to train model on
+        #     return FaceManager.Face(self.id_to_label[hash(str(encoding))], (x,y,w,h))
+        
+        #TODO compare encodings for close match
+        #TODO at beginning of facemanager: go through entire database and store encoding of each face. Attempt to compare distance of current embedding and stored ones
+        for i,id in enumerate(self.id_to_encoding.keys()): #for every id
+            if True in face_recognition.compare_faces(self.id_to_encoding[id], encoding, self.params.ENCODING_MATCH_TOLERANCE): #if one of the encodings match close enough
+                return FaceManager.Face(self.id_to_label[id], (x,y,w,h))
+
+        if str(encoding) in self.faces_to_audit.keys(): #exact match found pending audit
+            return FaceManager.Face(hash(str(encoding)), (x,y,w,h))
         #TODO compare encodings for close match pending audit
         else:
             self.faces_to_audit[str(encoding)] = frame_rgb[y:y+h, x:x+w] #add photo for auditing
             return FaceManager.Face("UNKNOWN", (x,y,w,h))
-        
